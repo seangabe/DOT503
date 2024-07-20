449eb0d38721731fcf0c28810f825c63adcce8a8
4935404 <4935404@STA-4935404>
2024-07-20 19:37:43 +0930

    Add project files.

---
+++ DOT503/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+using DOT503.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DOT503.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var model = new CalculatorModel();
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult Calculate(CalculatorModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                switch (model.Operator)
+                {
+                    case "+":
+                        model.Result = model.FirstNumber + model.SecondNumber;
+                        break;
+                    case "-":
+                        model.Result = model.FirstNumber - model.SecondNumber;
+                        break;
+                    case "*":
+                        model.Result = model.FirstNumber * model.SecondNumber;
+                        break;
+                    case "/":
+                        if (model.SecondNumber == 0)
+                        {
+                            ModelState.AddModelError("SecondNumber", "Division by zero is not allowed.");
+                        }
+                        else
+                        {
+                            model.Result = model.FirstNumber / model.SecondNumber;
+                        }
+                        break;
+                    default:
+                        ModelState.AddModelError("Operator", "Invalid operator.");
+                        break;
+                }
+            }
+            return View("Index", model);
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DOT503/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DOT503.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
